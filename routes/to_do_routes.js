d7ec68efba2520cd1c352b6071c1ec10aad46764
HP <thathsara.hewage99@gmail.com>
2022-08-17 19:37:16 +0530

    some changes of APIs

--- routes/to_do_routes.js
+++ routes/to_do_routes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const TaskController = require('../controllers/to_do_controller.js');
 
 router.get('/', TaskController.getAllTasks);
-router.post('/', TaskController.createNewTask);
-router.get('/:id', TaskController.findTaskById);
-router.patch('/:id', TaskController.updateATask);
-router.delete('/:id', TaskController.deleteATask);
+router.post('/add', TaskController.createNewTask);
+router.get('/find/:id', TaskController.findTaskById);
+router.patch('/list/:id', TaskController.updateATask);
+router.delete('/list/:id', TaskController.deleteATask);
 
 module.exports = router;
